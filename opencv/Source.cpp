e72b5d4cb64c2cffbfb70b45628ee82160b08183
TexasTrippin <90441307+TexasTrippin@users.noreply.github.com>
2021-09-10 10:36:15 +0300

    Add files via upload

--- opencv/Source.cpp
+++ opencv/Source.cpp
@@ -7,7 +7,9 @@ using namespace cv;
 using namespace std;
 int main()
 {
-	/*int height = 520;
+
+	/* //Блок рисования окна
+	int height = 520;
 	int width = 840;
 	Mat img(height, width, CV_8UC3);
 	Point textOrg(300, img.rows / 2);
@@ -17,8 +19,20 @@ int main()
 	putText(img, "Step By Step", textOrg, fontFace, fontScale, color);
 	namedWindow("Hello World", 0);
 		imshow("Hello World", img);*/
+	//Запрос от пользователя какой файл ему требуется открыть
+	setlocale(LC_ALL, "Russian");
+	string filename;
+	cout << "Имя файла ";
+	cin >> filename;
+	cout << "Ввели файл "<<filename<<endl;
+
+	
+
+
+
+	//Блок загрузки изображения
 	Mat img;
-	img = imread("img1.jpg", 1);
+	img = imread(filename, 1); //image.jpg
 	namedWindow("Hello World", WINDOW_AUTOSIZE);
 	imshow("Hello World", img);
 		waitKey(0);
