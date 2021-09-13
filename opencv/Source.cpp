f85eee4d2dd774ee9d7fb47c4205cda7c8e7ea35
TexasTrippin <90441307+TexasTrippin@users.noreply.github.com>
2021-09-13 10:06:43 +0300

    Update Source.cpp

--- opencv/Source.cpp
+++ opencv/Source.cpp
@@ -9,22 +9,22 @@ int main()
 {
 
 	 //Блок рисования окна
-	/*int height = 520; // задаем высоту (int - тип данных, целое)
-	int width = 840; // задаем ширину
-	Mat img(height, width, CV_8UC3); // Mat(создание матрицы) (CV_8UC3) 8-разрядная целочисленная матрица / изображение без знака с 3 каналами
-	Point textOrg(300, img.rows / 2); //Point (координаты x,y) , 
-	int fontFace = FONT_HERSHEY_SIMPLEX; //настройка шрифта , FONT_HERSHEY_SIMPLEX - тип шрифта
-	double fontScale = 1; //double - числовой тип данных , fontScale — переменная типа double, коэффициент масштабирования шрифта, умножаемый на базовый размер шрифта.
-	Scalar color(0, 0, 20); // Тип Scalar широко используется в OpenCV для передачи значений пикселей, цвет
-	putText(img, "Step By Step", textOrg, fontFace, fontScale, color); //  используется для рисования текстовой строки на любом изображении
-	namedWindow("Hello World", 0); // название окна
-		imshow("Hello World", img); */ //отображает изображение в градациях серого на рисунке. использует диапазон отображения по умолчанию для типа данных изображения и оптимизирует свойства рисунка, осей и объекта изображения для отображения изображения
+	//int height = 520; // задаем высоту (int - тип данных, целое)
+	//int width = 840; // задаем ширину
+	//Mat img(height, width, CV_8UC3); // Mat(создание матрицы) (CV_8UC3) 8-разрядная целочисленная матрица / изображение без знака с 3 каналами
+	//Point textOrg(300, img.rows / 2); //Point (координаты x,y) , 
+	//int fontFace = FONT_HERSHEY_SIMPLEX; //настройка шрифта , FONT_HERSHEY_SIMPLEX - тип шрифта
+	//double fontScale = 1; //double - числовой тип данных , fontScale — переменная типа double, коэффициент масштабирования шрифта, умножаемый на базовый размер шрифта.
+	//Scalar color(0, 0, 20); // Тип Scalar широко используется в OpenCV для передачи значений пикселей, цвет
+	//putText(img, "Step By Step", textOrg, fontFace, fontScale, color); //  используется для рисования текстовой строки на любом изображении
+	//namedWindow("Hello World", 0); // название окна
+	//	imshow("Hello World", img);  //отображает изображение в градациях серого на рисунке. использует диапазон отображения по умолчанию для типа данных изображения и оптимизирует свойства рисунка, осей и объекта изображения для отображения изображения
 	//Запрос от пользователя какой файл ему требуется открыть
-	setlocale(LC_ALL, "Russian"); //русификатор
-	string filename; //строчка с названием перменной 
-	cout << "Имя файла "; //вывод значения <<
-	cin >> filename;  // ввод значения >>
-	cout << "Ввели файл "<<filename<<endl; //endl текст будет выведен на след. строке 
+	//setlocale(LC_ALL, "Russian"); //русификатор
+	//string filename; //строчка с названием перменной 
+	//cout << "Имя файла "; //вывод значения <<
+	//cin >> filename;  // ввод значения >>
+	//cout << "Ввели файл "<<filename<<endl; //endl текст будет выведен на след. строке 
 
 	
 
@@ -32,10 +32,34 @@ int main()
 
 	//Блок загрузки изображения
 	Mat img; //создает матрицу
-	img = imread(filename, 1); //image.jpg (imread -  считывает изображение из файла, заданного , выводя формат файла из его содержимого.)
+	img = imread("image1.jpg", 1); //image.jpg (imread -  считывает изображение из файла, заданного , выводя формат файла из его содержимого.)
 	namedWindow("Hello World", WINDOW_AUTOSIZE); // название окна, авторазмер
 	imshow("Hello World", img); // отображает изображение в градациях серого на рисунке. использует диапазон отображения по умолчанию для типа данных изображения и оптимизирует свойства рисунка, осей и объекта изображения для отображения изображения
-		waitKey(0);
+	Mat src_gray;
+	Mat canny_output;
+	cvtColor(img, src_gray, COLOR_RGB2BGR);
+	blur(src_gray, src_gray, Size(3, 3));
+	/*double otsu_thresh_val = threshold(src_gray, img, 0, 255, THRESH_BINARY | THRESH_OTSU);*/
+	/*double high_thresh_val = otsu_thresh_val, lower_thresh_val = otsu_thresh_val * 0.5;*/
+	/*cout << otsu_thresh_val;*/
+	double lower_thresh_val=100, high_thresh_val= 300;
+	Canny(src_gray, canny_output,lower_thresh_val, high_thresh_val, 3);
+	/*char* source_grey_window = "Серое изображение";*/
+	namedWindow("Серое изображение", WINDOW_AUTOSIZE);
+	imshow("Серое изображение", canny_output);
+	imwrite("canny_output.jpg", canny_output);
+	waitKey(0);
 		system("pause");
 		return (0);
+
+
+
+	
+
+
+		
+
+
+
+
 }
