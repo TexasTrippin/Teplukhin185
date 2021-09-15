d201f2401df10d91aa8db3195f4a1631e8a4cc18
TexasTrippin <90441307+TexasTrippin@users.noreply.github.com>
2021-09-15 11:44:39 +0300

    Add files via upload

---
+++ тест/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mak1
+{
+    public class PasswordChecker
+    {
+        public static bool validatePassword(string password)
+        {
+            if (password.Length < 8 || password.Length > 20)
+                return false;
+            if (!password.Any(Char.IsLower))
+                return false;
+            if (!password.Any(Char.IsUpper))
+                return false;
+            if (!password.Any(Char.IsDigit))
+                return false;
+            if (password.Intersect("#$%^&_").Count() == 0)
+                return false;
+
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            bool arr = PasswordChecker.validatePassword("sasafsaasgsggsa342");
+            Console.WriteLine("res = " + arr);
+            //string password = 
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ тест/PasswordCheckerTests.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mak1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mak1.Tests
+{
+    [TestClass()]
+    public class PasswordCheckerTests
+    {
+        [TestMethod()]
+        public void Check_8Symbols_ReturnsTrue()
+        {
+            // Arrange
+
+            string password = "ASDqwe123$";
+            bool expected = true;
+
+            // Act
+            bool actual = PasswordChecker.validatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Check_4Symbols_ReturnsFalse()
+        {
+            // Arrange
+
+            string password = "Qw$1";
+            bool expected = false;
+
+            // Act
+            bool actual = PasswordChecker.validatePassword(password);
+
+            // Assert
+            Assert.IsFalse (actual);
+        }
+        [TestMethod()]
+        public void Check_21Symbols_ReturnsFalse()
+        {
+            // Arrange
+
+            string password = "Qwertyuiodsadasdsada$";
+            bool expected = false;
+
+            // Act
+            bool actual = PasswordChecker.validatePassword(password);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void Numbers_ReturnsTrue()
+        {
+            // Arrange
+
+            string password = "Fsjafjasgfjsa123#";
+            bool expected = true;
+
+            // Act
+            bool actual = PasswordChecker.validatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Check_NumbersNot_ReturnsFalse()
+        {
+            // Arrange
+
+            string password = "Fsjafjasgfjsa";
+            bool expected = false;
+
+            // Act
+            bool actual = PasswordChecker.validatePassword(password);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+        [TestMethod()]
+        public void SpecialSymbols_ReturnsTrue()
+        {
+            // Arrange
+
+            string password = "Djsmiavhnu$123";
+            bool expected = true;
+
+            // Act
+            bool actual = PasswordChecker.validatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SpecialSymbolsNot_ReturnsFalse()
+        {
+            // Arrange
+
+            string password = "Djsmiavhnu";
+            bool expected = false;
+
+            // Act
+            bool actual = PasswordChecker.validatePassword(password);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+        [TestMethod()]
+        public void ZaglavnBukva_ReturnsTrue()
+        {
+            // Arrange
+
+            string password = "Fdasdds123%";
+            bool expected = true;
+
+            // Act
+            bool actual = PasswordChecker.validatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void ZaglavnBukvaNot_ReturnsFalse()
+        {
+            // Arrange
+
+            string password = "dgdsjgiasiny";
+            bool expected = false;
+
+            // Act
+            bool actual = PasswordChecker.validatePassword(password);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+        [TestMethod()]
+        public void StrochnieBukvi_ReturnsTrue()
+        {
+            // Arrange
+
+            string password = "Fdasdds123%";
+            bool expected = true;
+
+            // Act
+            bool actual = PasswordChecker.validatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void StrochnieBukviNot_ReturnsFalse()
+        {
+            // Arrange
+
+            string password = "FADADFAFA";
+            bool expected = false;
+
+            // Act
+            bool actual = PasswordChecker.validatePassword(password);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}
