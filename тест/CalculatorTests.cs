bdda00ca79b249636fba6ee7e7d5bb74d6330b4e
TexasTrippin <90441307+TexasTrippin@users.noreply.github.com>
2021-09-17 12:14:42 +0300

    Add files via upload

---
+++ тест/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mak1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mak1.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void CalculateTest()
+        {
+            double res = Calculator.Calculate(6, 2, "+");
+            Console.WriteLine("res = " + res);
+
+            return false;
+    }
+}
---
+++ тест/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mak1
+{
+    public class Calculator
+    {
+        public static double Calculate(double n1, double n2, string operation)
+        {
+
+
+            if (operation == "+")
+            {
+                return n1 + n2;
+            }
+            else if (operation == "-")
+            {
+                return  n1 - n2;
+            }
+            else if (operation == "*")
+            {
+                return n1 * n2;
+            }
+            else if (operation == "/")
+            {
+                if (n2 != 0)
+                    return n1 / n2;
+                else Console.WriteLine("Неизвестный оператор");
+            }
+
+
+
+            return 0;
+
+
+
+        }
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Введите первое число:");
+            double n1 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("\nВведите оператор:");
+            string operation = Console.ReadLine();
+
+            Console.Write("\nВведите второе число:");
+            double n2 = Int32.Parse(Console.ReadLine());
+
+            double res = Calculator.Calculate(n1, n2, operation );
+            Console.WriteLine("Результат = " + res);
+
+            
+
+        }
+    }
+}
