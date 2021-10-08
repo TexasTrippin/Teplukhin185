0a5fcbce524bda22642f681bd01515ab6a315311
Maxim Revnivtsev <max_rev1380@mail.ru>
2021-10-08 09:37:59 +0300

    wpf

---
+++ worldskills/wpf/Tours/HotelsPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tours
+{
+    /// <summary>
+    /// Логика взаимодействия для HotelsPage.xaml
+    /// </summary>
+    public partial class HotelsPage : Page
+    {
+        public HotelsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage()); 
+        }
+
+        /*private void Button_Click_OpenCV(object sender, RoutedEventArgs e)
+        {
+        
+        }
+
+        private void Button_Click_Test(object sender, RoutedEventArgs e)
+        {
+            
+        }*/
+    }
+}
