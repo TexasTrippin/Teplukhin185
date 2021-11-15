5b269bf48aae2cd30a6bd7351dfbab38ed358138
TexasTrippin <90441307+TexasTrippin@users.noreply.github.com>
2021-11-15 10:44:06 +0300

    Add files via upload

---
+++ worldskills/import_bd/Tours/HotelsPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Tours
+{
+    /// <summary>
+    /// Логика взаимодействия для HotelsPage.xaml
+    /// </summary>
+    public partial class HotelsPage : Page
+    {
+        public HotelsPage()
+        {
+            InitializeComponent();
+            DGridHotels.ItemsSource = Entity.GetContext().Hotel.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new OpenCV());
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new Test());
+        }
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Hotel));
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var hotelsForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {hotelsForRemoving.Count()} элементов?", "Внимание",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    Entity.GetContext().Hotel.RemoveRange(hotelsForRemoving);
+                    Entity.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+                    DGridHotels.ItemsSource = Entity.GetContext().Hotel.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                Entity.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = Entity.GetContext().Hotel.ToList();
+            }
+        }
+    }
+}
---
+++ worldskills/import_bd/Tours/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tours
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new HotelsPage());
+            Manager.MainFrame = MainFrame;
+
+            ImportTours();
+        }
+        
+        private void ImportTours()
+        {
+            var fileData = File.ReadAllLines(@"C:\Users\Администратор\Desktop\мусор\for\Touurs.txt");
+            var images = Directory.GetFiles(@"C:\Users\Администратор\Desktop\мусор\for\tours");     
+
+
+            foreach (var line in fileData)
+            {
+                var data = line.Split('\t');
+
+                var tempTour = new Tour
+                {
+                    Name = data[0].Replace("\"", ""),
+                    TicketCount = int.Parse(data[2]),
+                    Price = decimal.Parse(data[3]),
+                    IsActual = (data[4] == "0") ? false : true
+                };
+
+                foreach (var tourType in data[5].Split(new string[] {" ," }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var currentType = Entity.GetContext().Type.ToList().FirstOrDefault(p => p.Name == tourType);
+                    if (currentType != null)
+                        tempTour.Type.Add(currentType);
+                }
+                try
+                {
+                    tempTour.ImagePreview = File.ReadAllBytes(images.FirstOrDefault(p => p.Contains(tempTour.Name)));
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Entity.GetContext().Tour.Add(tempTour);
+                Entity.GetContext().SaveChanges();
+            }
+
+
+        }
+
+
+
+
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                BtnBack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BtnBack.Visibility = Visibility.Hidden;
+            }
+             
+   
+            
+          
+        }
+    }
+}
