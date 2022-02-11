893d8ff80507b21cd8de82284d4acc8e3938467f
DarkGoldFOX <90600446+DarkGoldFOX@users.noreply.github.com>
2022-02-11 11:57:31 +0300

    123

---
+++ dme/App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CoreLib;
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Utils.GenNumOrder("Иванов Иван Иванович", DateTime.Now));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dme/CoreLib/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib
+{
+    public class Utils
+    {
+        public static string GenNumOrder(string client, DateTime date)
+        {
+            var fullName = client.Split(' ');
+            return $"{fullName[0]} {fullName[1].Substring(0, 1)}.{fullName[2].Substring(0, 1)}._{date.ToString("MM.dd.yyyy")}_{date.ToString("HH")}_{date.ToString("mm")}";
+        }
+    }
+}
