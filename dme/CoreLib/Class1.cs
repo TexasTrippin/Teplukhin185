057d49152ca9fde4aa095548e1309be46803a4f1
DarkGoldFOX <90600446+DarkGoldFOX@users.noreply.github.com>
2022-02-11 12:26:28 +0300

    123

--- dme/CoreLib/Class1.cs
+++ dme/CoreLib/Class1.cs
@@ -13,5 +13,78 @@ namespace CoreLib
             var fullName = client.Split(' ');
             return $"{fullName[0]} {fullName[1].Substring(0, 1)}.{fullName[2].Substring(0, 1)}._{date.ToString("MM.dd.yyyy")}_{date.ToString("HH")}_{date.ToString("mm")}";
         }
+        public static string CheckPassword(string Password)
+        {
+            if (Password.Length < 6 && Password.Length > 10)
+            {
+                return "Пароль должен быть не менее 6 и не более 10 букв";
+            }
+            else
+            {
+                bool isSpec = false;
+                foreach (var item in Password)
+                {
+                    if ("-*|$".Contains(item))
+                    {
+                        isSpec = true;
+                    }
+                }
+                bool isDigit = false;
+                foreach (var item in Password)
+                {
+                    if (char.IsDigit(item))
+                    {
+                        isDigit = true;
+                    }
+                }
+                if (isDigit == false)
+                {
+                    return "Не содержит спец символов";
+
+                }
+                bool IsNumber = false;
+                foreach (var item in Password)
+                {
+                    if (char.IsNumber(item))
+                    {
+                        IsNumber = true;
+                    }
+                }
+                if (IsNumber == false)
+                {
+                    return "Не содержит цифр";
+                }
+                bool isUpper = false;
+                foreach (var item in Password)
+                {
+                    if (char.IsUpper(item))
+                    {
+                        isUpper = true;
+                    }
+                }
+                if (isUpper == false)
+                {
+                    return "Не содержит больших букв";
+                }
+                bool Islower = false;
+                foreach (var item in Password)
+                {
+                    if (char.IsLower(item))
+                    {
+                        Islower = true;
+                    }
+                }
+                if (Islower == false)
+                {
+                    return "Не содержит маленьких букв";
+                }
+                if (isSpec == false)
+                {
+                    return "Не содержит спец символов";
+                }
+            }
+            return "Хороший пароль";
+            }
+        }
     }
-}
+
---
+++ dme/CoreLibTests/UtilsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CoreLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib.Tests
+{
+    [TestClass()]
+    public class UtilsTests
+    {
+        [TestMethod()]
+        public void CheckPasswordTestTrue()
+        {
+            string password = "Admin1*df";
+            string expected = "Хороший пароль";
+            string actual = Utils.CheckPassword(password);
+            Assert.AreEqual(expected, actual); 
+        }
+        [TestMethod()]
+        public void CheckPasswordTestFalse ()
+        {
+            string password = "Aq1$";
+            bool expected = false;
+            string actual = Utils.CheckPassword(password);
+            Assert.IsFalse(expected, actual);
+        }
+    }
+}
